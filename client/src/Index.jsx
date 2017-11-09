420704302a5b01625ef0d8f7517613110743b0b9
Manjhunath Ravi <manjhunath.kr@gmail.com>
2017-11-09 01:18:31 -0700

    giving api call request to server with new todo

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoList from './components/TodoList.jsx'
+import axios from 'axios';
 
 
 class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            newTodo: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.submitTodo = this.submitTodo.bind(this);
+        this.updateTodos = this.updateTodos.bind(this);
+    }
+
+
+    updateTodos(){
+        axios.post('/', {params:{ newTodo: this.state.newTodo}})
+        .then(function(res) {
+            console.log(res);
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+    }
+
+    handleChange(e){  
+        this.setState({newTodo: e.target.value});
+        
+    }
+
+    submitTodo(e){
+        e.preventDefault();
+        this.inputForm.value= "";
+        alert("Submit clicked");
+        this.updateTodos();
+        
+    }
+
+    componentDidUpdate(){
+        console.log("Handling change " + this.state.newTodo );
+    }
+
     render(){
         return(
         <div> 
-            Hey I'm in my React App component!!!
+            <form onSubmit={this.submitTodo}>
+                <div>
+                    <label>Enter Todo:   
+                        <input id="enter-todo" type="text" onChange={this.handleChange} ref={(element)=>{this.inputForm = element}}/>
+                    </label>
+                </div>
+                <div>
+                    <input id="submit" type="submit" value="Submit"/>
+                </div>
+                
+            </form>
+            
         </div>);
     }
 }
--- server/index.js
+++ server/index.js
@@ -3,8 +3,17 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var port = 5000;
+var router = express.Router();
+// var user = require('/user')
 
-app.use(express.static(path.join(__dirname, '/../client/dist/')))
+app.use(express.static(path.join(__dirname, '/../client/dist/')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.post('/', function(req, res){
+    console.log("came to post / ", req.body.params.newTodo);
+    res.send(req.body.params.newTodo);
+})
 
 app.listen(port, () => {
     console.log("listening to port: ", port);
